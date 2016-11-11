a8f9bb4da9d1e433af0f15c330a85f34d2c78a65
Robin Chatfield <robin.chatfield@gmail.com>
2016-11-11 15:26:43 +0100

    odoo-sale: Adapted module to 9.0.

--- stock_credit_check/__openerp__.py
+++ stock_credit_check/__openerp__.py
@@ -29,7 +29,7 @@
 """,
     'author': 'Vertel AB',
     'website': 'http://www.vertel.se',
-    'depends': ['stock',],
+    'depends': ['stock', 'partner_show_credit'],
     'data': [
         'stock_view.xml',
     ],
