68f8cbdc061fd23036a725b2a0d24cbe05ec47a5
Robin Chatfield <robin.chatfield@vertel.se>
2019-06-19 17:05:14 +0200

    sale_order_line_double_warning: Improved warning message. Added logging when creating sale order lines (remember to remove it later, because it will hog resources).

--- sale_order_line_double_warning/sale.py
+++ sale_order_line_double_warning/sale.py
@@ -21,12 +21,35 @@
 
 from openerp import api, models, fields, _
 from openerp.exceptions import Warning
+import traceback
+
 import logging
 _logger = logging.getLogger(__name__)
 
 class sale_order_warning_wizard(models.TransientModel):
     _name = 'sale.order.warning.wizard'
-
+    
+    @api.model
+    def default_order_id(self):
+        return self.env['sale.order'].browse(self._context.get('active_id'))
+        
+    @api.one
+    @api.depends('order_id')
+    def compute_line_ids(self):
+    # ~ def default_line_ids(self):
+        # ~ order_id = self.env['sale.order'].browse(self._context.get('active_id'))
+        order_id = self.order_id
+        line_ids = self.env['sale.order.line'].browse()
+        for product in order_id.order_line.mapped('product_id'):
+            lines = order_id.order_line.filtered(lambda l: l.product_id == product)
+            if len(lines) > 1:
+                line_ids += lines
+        self.line_ids = line_ids
+    
+    order_id = fields.Many2one(comodel_name='sale.order', string='Order', default=default_order_id, readonly=True)
+    # ~ line_ids = fields.Many2many(comodel_name='sale.order.line', string='Order Lines', default=default_line_ids, readonly=True)
+    line_ids = fields.Many2many(comodel_name='sale.order.line', string='Order Lines', compute='compute_line_ids')
+    
     @api.multi
     def confirm(self):
         order = self.env['sale.order'].browse(self._context.get('active_id'))
@@ -51,3 +74,17 @@ class sale_order(models.Model):
             }
         else:
             return self.action_button_confirm()
+
+class SaleOrderLine(models.Model):
+    _inherit ='sale.order.line'
+    
+    @api.model
+    @api.returns('self', lambda value: value.id)
+    def create(self, vals):
+        res = super(SaleOrderLine, self).create(vals)
+        if res.product_id and (len(res.order_id.order_line.filtered(lambda l: l.product_id == res.product_id)) > 1):
+            tag = 'sale.order.line double trouble!! %s' % res.id
+            tb = traceback.format_stack()
+            msg = ('\n%s' % tag) + ''.join([line.replace('\n', '\n%s' % tag) for line in tb])
+            _logger.warn(msg)
+        return res
