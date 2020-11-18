eadc2ee078bd748703bc8ca92a0f1b8414c5c88e
Artur Chilangwa <artur.chilangwa@vertel.se>
2020-11-18 16:47:53 +0100

    AFC-1306: Rename fenix_test_data to sale_test-data-fenix

--- sale_test-data_fenix/__manifest__.py
+++ sale_test-data_fenix/__manifest__.py
@@ -1,7 +1,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 {
-    "name": "Fenix Test Sales",
+    "name": "Fenix Sale Test Data",
     "version": "12.0.1.0.1",
     "author": "Vertel AB",
     "license": "AGPL-3",
@@ -17,8 +17,8 @@
     "data": [
         'data/product_product.xml',
         'data/sale_order.xml',
-
     ],
+    
     "application": True,
     "installable": True,
 }
