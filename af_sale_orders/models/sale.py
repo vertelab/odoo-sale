827dc9398edae03de32be0e066d8d3df730dfba3
Rupareliya Hemangi <rupareliyahemangi145@gmail.com>
2020-11-21 17:28:50 +0530

    [ADD] af_sale_orders: AFC-1278 Updated code for changes.

--- af_sale_orders/models/sale.py
+++ af_sale_orders/models/sale.py
@@ -6,32 +6,127 @@ class SaleOrder(models.Model):
     @api.multi
     def action_confirm(self):
         carreer_product_id = self.env.ref('af_sale_orders.kvd_01').id
-        interpretor_product_ids = [self.env.ref('af_sale_orders.kvd_tlk_01').id,
-                      self.env.ref('af_sale_orders.kvd_tlk_02').id]
-        is_interpretor = False
         is_carreer = False
         for line in self.order_line:
             if line.product_id and line.product_id.id == carreer_product_id:
                 is_carreer = True
-            elif line.product_id and line.product_id.id in interpretor_product_ids:
-                is_interpretor = True
+        to_do_stage_id = self.env.ref('project.project_stage_0').id
+        optional_stage_id = self.env.ref('af_sale_orders.project_optional_stage').id
+        stage_ids = [self.env.ref('project.project_stage_0'), self.env.ref('project.project_stage_2'),
+                     self.env.ref('af_sale_orders.project_optional_stage')]
         if is_carreer:
             project = self.env['project.project'].create({
                 'name': self.partner_id.name
             })
-            if is_interpretor:
-                task_obj = self.env['project.task']
-                task_obj.create({
-                    'name': "Task1",
-                    'project_id': project.id
-                })
-                task_obj.create({
-                    'name': "Task2",
-                    'project_id': project.id
-                })
-                task_obj.create({
-                    'name': "Task3",
-                    'project_id': project.id
-                })
-                
+            for stage in stage_ids:
+                stage.project_ids = [(4, project.id)]
+            task_obj = self.env['project.task']
+            task_obj.create({
+                'name': "Task1",
+                'project_id': project.id,
+                'task_number': 1,
+                'stage_id': to_do_stage_id,
+                'required_task': True,
+                'color': 1,
+                'instruction': 'Instruction 1'
+            })
+            task_obj.create({
+                'name': "Task2",
+                'project_id': project.id,
+                'task_number': 2,
+                'stage_id': to_do_stage_id,
+                'required_task': True,
+                'color': 1,
+                'instruction': 'Instruction 2'
+            })
+            task_obj.create({
+                'name': "Task3",
+                'project_id': project.id,
+                'task_number': 3,
+                'stage_id': to_do_stage_id,
+                'required_task': True,
+                'color': 1,
+                'instruction': 'Instruction 3'
+            })
+            task_obj.create({
+                'name': "Task4",
+                'project_id': project.id,
+                'task_number': 4,
+                'stage_id': to_do_stage_id,
+                'required_task': True,
+                'color': 1,
+                'instruction': 'Instruction 4'
+            })
+            task_obj.create({
+                'name': "Task5",
+                'project_id': project.id,
+                'task_number': 5,
+                'stage_id': to_do_stage_id,
+                'required_task': True,
+                'color': 1,
+                'instruction': 'Instruction 5'
+            })
+            task_obj.create({
+                'name': "Task6",
+                'project_id': project.id,
+                'task_number': 6,
+                'stage_id': to_do_stage_id,
+                'required_task': True,
+                'color': 1,
+                'instruction': 'Instruction 6'
+            })
+            task_obj.create({
+                'name': "Task7",
+                'project_id': project.id,
+                'task_number': 7,
+                'stage_id': to_do_stage_id,
+                'required_task': True,
+                'color': 1,
+                'instruction': 'Instruction 7'
+            })
+            task_obj.create({
+                'name': "Task8",
+                'project_id': project.id,
+                'task_number': 8,
+                'stage_id': to_do_stage_id,
+                'required_task': True,
+                'color': 1,
+                'instruction': 'Instruction 8'
+            })
+            task_obj.create({
+                'name': "Optional-Task1",
+                'project_id': project.id,
+                'task_number': 1,
+                'stage_id': optional_stage_id,
+                'optional_task': True,
+                'color': 11,
+                'instruction': 'Instruction 1'
+            })
+            task_obj.create({
+                'name': "Optional-Task2",
+                'project_id': project.id,
+                'task_number': 2,
+                'stage_id': optional_stage_id,
+                'optional_task': True,
+                'color': 11,
+                'instruction': 'Instruction 3'
+            })
+            task_obj.create({
+                'name': "Optional-Task3",
+                'project_id': project.id,
+                'task_number': 3,
+                'stage_id': optional_stage_id,
+                'optional_task': True,
+                'color': 11,
+                'instruction': 'Instruction 3'
+            })
+            task_obj.create({
+                'name': "Optional-Task4",
+                'project_id': project.id,
+                'task_number': 4,
+                'stage_id': optional_stage_id,
+                'optional_task': True,
+                'color': 11,
+                'instruction': 'Instruction 4'
+            })
         return super(SaleOrder, self).action_confirm()
