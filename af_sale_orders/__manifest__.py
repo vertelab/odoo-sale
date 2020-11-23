cc0f6e1021463e78b0068347b16b8298bdfaf8e3
Rupareliya Hemangi <rupareliyahemangi145@gmail.com>
2020-11-23 10:59:25 +0530

    [ADD] af_sale_orders: AFC-1278 Updatd code in af_sale_orders for new modifications.

--- af_sale_orders/__manifest__.py
+++ af_sale_orders/__manifest__.py
@@ -1,19 +1,21 @@
 {
     'name': 'AF Sale Orders',
-    'version': '12.0.0.0',
+    'version': '12.0.0.1',
     'author': 'Vertel AB',
     'license': 'AGPL-3',
     'website': 'http://www.vertel.se',
     'category': 'Sale',
     'summary': 'Price-list with links to project and activities.',
     'description': "This module adds 'Carreer advice' and 'Interpretor' products. When Sale order approves"
-                   "with these products created Project and Tasks.",
+                   "with 'Carreer advice' created Project and Tasks.Also adds some fileds like start date, end date"
+                   "and task numbers functionality in project and tasks.",
     'depends': [
         'sale',
         'project',
     ],
     'data': [
         'data/product_template.xml',
+        'views/project_views.xml'
     ],
     'installable': True,
     'auto_install': False,
