1c8931d2bbc28bd9ba06f0ee86183be3194cd4c4
Rupareliya Hemangi <rupareliyahemangi145@gmail.com>
2020-11-28 18:04:24 +0530

    [ADD] af_sale_orders: AFC-1278 Worked on new modifications.

--- af_sale_orders/models/project.py
+++ af_sale_orders/models/project.py
@@ -7,6 +7,7 @@ class ProjectProject(models.Model):
 
     start_date = fields.Date("Start Date")
     end_date = fields.Date("End Date")
+    sale_order_id = fields.Many2one('sale.order')
 
 class ProjectTask(models.Model):
 
@@ -25,8 +26,10 @@ class ProjectTask(models.Model):
 
     @api.onchange('parent_task_id')
     def onchange_parent_task_id(self):
+        to_do_stage_id = self.env.ref('project.project_stage_0').id
         if self.parent_task_id:
-            self.color = 10
+            self.color = self.parent_task_id.color
+            self.stage_id = to_do_stage_id
         else:
             self.color = 0
 
@@ -35,7 +38,6 @@ class ProjectTask(models.Model):
         for task in self:
             if task.id not in sub_tasks_list:
                 if task.parent_task_id:
-                    task.color = 10
                     parent_task = task.parent_task_id
                     sub_tasks = self.search([('parent_task_id', '=', parent_task.id)])
                     if parent_task.task_number:
@@ -54,9 +56,14 @@ class ProjectTask(models.Model):
     @api.model
     def create(self, vals):
         res = super(ProjectTask, self).create(vals)
+        if 'from_sale_order' not in self._context and not vals.get('optional_task') and \
+                not vals.get('parent_task_id') and res.project_id and res.project_id.sale_order_id:
+            raise Warning(_("You can't create Parent Task!"))
+        todo_stage = self.env.ref('project.project_stage_0')
         if vals.get('parent_task_id'):
-            res.color = 10
+            res.stage_id = todo_stage.id
             parent_task = self.env['project.task'].browse(vals.get('parent_task_id'))
+            res.color = parent_task.color
             sub_tasks = self.search([('parent_task_id', '=', parent_task.id), ('id', '!=', res.id)])
             sub_tasks_len = len(sub_tasks)
             if parent_task.task_number:
@@ -88,15 +95,26 @@ class ProjectTask(models.Model):
     def write(self, vals):
         res = super(ProjectTask, self).write(vals)
         task_obj = self.env['project.task']
+        done_stage = self.env.ref('project.project_stage_2')
+        todo_stage = self.env.ref('project.project_stage_0')
         for task in self:
+            if task.optional_task and task.stage_id and task.stage_id.id == todo_stage.id:
+                raise Warning(_("Can not move Optional task to Todo stage!"))
+            if vals.get('stage_id'):
+                if task.stage_id and task.stage_id.name == 'Done':
+                    subtasks = self.search([('parent_task_id', '=', task.id)])
+                    for sub_task in subtasks:
+                        if not sub_task.stage_id.id == done_stage.id:
+                            raise Warning(_("You can't move this task to Done as its subtask %s is not Done" % sub_task.name))
+                        sub_task.stage_id = done_stage.id
             if vals.get('name') and task.parent_task_id and not vals.get('parent_task_id'):
                 number = task.task_name_number.split(' ')
                 if len(number) > 1:
                     number = number[0]
                     task.task_name_number = number + ' ' + task.name
             if vals.get('parent_task_id'):
-                task.color = 10
                 parent_task = task_obj.browse(vals.get('parent_task_id'))
+                task.color =parent_task.color
                 sub_tasks = self.search([('parent_task_id', '=', parent_task.id), ('id', '!=', task.id)])
                 sub_tasks_len = len(sub_tasks)
                 if parent_task.task_number:
@@ -127,7 +145,32 @@ class ProjectTask(models.Model):
 
     @api.multi
     def unlink(self):
+        done_stage = self.env.ref('project.project_stage_2')
         for task in self:
+            subtasks = self.search([('parent_task_id', '=', task.id)])
+            if subtasks:
+                raise Warning("You can't remove Parent Task!")
             if task.required_task:
                 raise Warning(_("You can't remove this task as this is required task!"))
+            if task.parent_task_id and task.stage_id and task.parent_task_id.stage_id and \
+                    (task.stage_id.id == done_stage.id or task.parent_task_id.stage_id.id == done_stage.id):
+                raise Warning(_("You can't remove task which is Done or Parent Task is Done!"))
         return super(ProjectTask, self).unlink()
+
+class TaskStages(models.Model):
+    _inherit = 'project.task.type'
+
+    @api.multi
+    def name_get(self):
+        vals = []
+        context = self._context
+        for record in self:
+            if 'search_default_project_id' in context:
+                name = record.name
+                if record.description:
+                    name += ' - '
+                    name += record.description
+            else:
+                name = record.name
+            vals.append((record.id, name))
+        return vals
