7de68593777644cbd156855187096e340560f6ef
Marcus Jakobsson <jakmar@odooutv14.vertel.se>
2022-02-07 13:30:05 +0000

    Added a module that fixes the sale order wizard, we were unable to create invoices for some reason

---
+++ sale_order_fix_create_invoice_wizard/__manifest__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Fix Sale Order Wizard",
+
+    'summary': """
+        Fix Sale Order Wizard""",
+
+    'description': """
+       Fix Sale Order Wizard
+    """,
+
+    'author': "Vertel AB",
+    'website': "www.vertel.se",
+
+    # Categories can be used to filter modules in modules listing
+    'category': 'sale',
+    'version': '14.0.0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base', 'sale'],
+
+    # always loaded
+    'data': [
+        #'views/sale_inherited.xml',
+    ],
+}
