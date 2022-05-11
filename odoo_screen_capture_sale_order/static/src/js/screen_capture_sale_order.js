83ccf57bae13d21f2b05ff36e5e232c35e1eb966
Daniel Löfgren <dan.lofgrens@gmail.com>
2022-05-11 14:04:38 +0000

    Improved js code, too odoo standards

--- odoo_screen_capture_sale_order/static/src/js/screen_capture_sale_order.js
+++ odoo_screen_capture_sale_order/static/src/js/screen_capture_sale_order.js
@@ -1,48 +1,40 @@
-window.addEventListener('load', function () {
-    // Everything has loaded!
+odoo.define("odoo_screen_capture_sale_order.screen_capture_sale_order", function (require) {
+    "use strict";
 
-    const element = document.querySelector('#portal_sale_content')
-    const title_element = document.querySelector('.my-0')
+    var publicWidget = require('web.public.widget')
 
-    const button_element = $(".o_download_btn")
+    publicWidget.registry.screenCaptureSaleOrder = publicWidget.Widget.extend({
+        selector: '#wrapwrap',
+        read_events: {
+            'click .o_download_btn': '_fetch_pdf',
+        },
 
-    button_element.on("click", function () {
+        _fetch_pdf: function () {
+            const element = document.querySelector('#portal_sale_content')
+            const title_element = document.querySelector('.my-0')
 
-        let title = ""
-        let formatted_title = title_element.textContent.trim().split('\n')
+            let title = ""
+            let formatted_title = title_element.textContent.trim().split('\n')
+            let i = 0
 
-        for (i in formatted_title) {
-            if (i > 0) {
-                title += " "
+            for (i in formatted_title) {
+                if (i > 0) {
+                    title += " "
+                }
+                title += formatted_title[i].trim()
             }
-            title += formatted_title[i].trim()
+
+            let width = element.offsetWidth * 1.65;
+            let height = element.offsetHeight * 1.25;
+            let opt = {
+                margin: 25,
+                filename: title,
+                image: { type: 'jpeg', quality: 0.98 },
+                html2canvas: { scale: 1 },
+                jsPDF: { unit: 'px', format: [width, height], orientation: 'portrait' }
+            };
+
+            html2pdf().set(opt).from(element).toContainer().toCanvas().toImg().toPdf().save()
         }
-
-        var width = element.offsetWidth + 350;
-        var height = element.offsetHeight + 250;
-        var opt = {
-            margin: 25,
-            filename: title,
-            image: { type: 'jpeg', quality: 0.98 },
-            html2canvas: { scale: 1 },
-            jsPDF: { unit: 'px', format: [width, height], orientation: 'portrait' }
-        };
-
-        html2pdf().set(opt).from(element).toContainer().toCanvas().toImg().toPdf().save()
-        //.then(cleanup())
     })
-});
-
-function cleanup() {
-    the_node = document.querySelector('#wrapwrap').style.zIndex = "1001";
-    // setTimeout(() => {
-    //     const elements = ['.html2canvas-container', '.html2pdf__overlay', '.html2pdf__container']
-
-    //     elements.forEach((n) => {
-    //         node = document.querySelector(n);
-    //         console.log(node)
-    //         node.remove()
-    //     })
-    //     debugger
-    // }, 5000)
-}
+})
