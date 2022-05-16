c66b869460c717f059b3f77eb9290cabcb793410
Marcus Jakobsson <marcus.jakobsson@vertel.se>
2022-05-16 08:59:21 +0000

    sale_project_task_description:added a translation

--- sale_project_task_description/models/project.py
+++ sale_project_task_description/models/project.py
@@ -4,3 +4,4 @@ from odoo import models, fields, _, api
 class ProjectTask(models.Model):
     _inherit = 'project.task'
     object_description = fields.Char(string="Objektbeskrivning")
+    product_id = fields.Many2one(related='sale_line_id.product_id')
