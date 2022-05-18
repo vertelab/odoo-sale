f9c08c0d7f2b3b68d0de8bccc46be4135ecbb58a
Marcus Jakobsson <jakmar@odooutv14.vertel.se>
2022-05-18 07:44:20 +0000

    Changes to a view that broke azzar env

--- sale_project_task_description/models/sale_order.py
+++ sale_project_task_description/models/sale_order.py
@@ -1,12 +1,12 @@
 from odoo import models, fields, _, api
 
-class ProductTemplate(models.Model):
-    _inherit = 'product.template'
-    work_description = fields.Char(string="Work Description")
+# ~ class ProductTemplate(models.Model):
+    # ~ _inherit = 'product.template'
+    # ~ work_description = fields.Char(string="Work Description")
 
-class ProductProduct(models.Model):
-    _inherit = 'product.product'
-    work_description = fields.Char(string="Work Description")
+# ~ class ProductProduct(models.Model):
+    # ~ _inherit = 'product.product'
+    # ~ work_description = fields.Char(string="Work Description")
     
 
 class SaleOrder(models.Model):
@@ -20,11 +20,11 @@ class SaleOrderLine(models.Model):
     
     work_description = fields.Char(string="Work Description")
     
-    @api.onchange('product_id')
-    def product_id_change_set_work_description(self):
-        for record in self: 
-            if record.product_id.work_description:
-                record.work_description = record.product_id.work_description
+    # ~ @api.onchange('product_id')
+    # ~ def product_id_change_set_work_description(self):
+        # ~ for record in self: 
+            # ~ if record.product_id.work_description:
+                # ~ record.work_description = record.product_id.work_description
     
     def _timesheet_create_task_prepare_values(self, project):
         res = super(SaleOrderLine, self)._timesheet_create_task_prepare_values(project)
