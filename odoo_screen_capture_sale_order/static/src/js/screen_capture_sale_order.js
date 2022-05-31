330d1a3695c212ad75084df118625f46cfb66f4c
Miracle <miracleaayodele@gmail.com>
2022-05-31 07:42:38 +0000

    fixed contents to fit in page

--- odoo_screen_capture_sale_order/static/src/js/screen_capture_sale_order.js
+++ odoo_screen_capture_sale_order/static/src/js/screen_capture_sale_order.js
@@ -10,19 +10,39 @@ odoo.define("odoo_screen_capture_sale_order.screen_capture_sale_order", function
         },
 
         _fetch_pdf: function () {
+            /* TODO: 
+                properly fit contents in pdf using page break 
+                html2pdf library suggests <div class="html2pdf__page-break"/> to be added before the element you want to break into the next page
+
+                PROBLEM: How to determine the contents on each page, the previous page and next page, this will help to know which page to apply page-break on
+
+                SUGGESTED SOLUTION: 
+
+            */
+
             const title_element = document.querySelector('.my-0')
 
-            let elems = document.querySelectorAll("#portal_sale_content > div > .card-body > div[data-oe-model='sale.order']") // when there is a user logged in
+            let header_information = document.querySelectorAll("#portal_sale_content > div > .card-body > #informations > div.row > div > span")
+            if (header_information.length > 0) {
+                Array.from(header_information).forEach( function (el) {
+                    const section_div = el.querySelectorAll("section > div.container")    
+                    Array.from(section_div).forEach( function (section_div_el) {
+                        section_div_el.classList.remove('container')
+                    })
+                });
+            }
+
+            // when there is a user logged in
+            let elems = document.querySelectorAll("#portal_sale_content > div > .card-body > div[data-oe-model='sale.order'], div[data-oe-model='sale.order.line'], div[data-oe-model='sale.order.option']")
+
             if (elems.length === 0) {
                 elems = document.querySelectorAll("#portal_sale_content > div > .card-body > .oe_no_empty") // when there is no user logged in
             }
 
             Array.from(elems).forEach( function (el) {
                 const section_div = el.querySelectorAll("section > div.container")
-
                 Array.from(section_div).forEach( function (section_div_el) {
                     section_div_el.classList.remove('container')
-                    
                 })
             });
 
@@ -44,7 +64,7 @@ odoo.define("odoo_screen_capture_sale_order.screen_capture_sale_order", function
                 margin: [5, 5, 10, 5], //top, left, buttom, right
                 filename: title,
                 image: { type: 'jpeg', quality: 0.98 },
-                html2canvas: { scale: 1},
+                html2canvas: { scale: 2, logging: true, dpi: 192, letterRendering: true},
                 jsPDF: { unit: 'mm', format: ['210', '297'], orientation: 'portrait' },
             };
 
