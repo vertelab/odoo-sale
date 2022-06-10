a015c97c955768b9cd34b7f4db7745e51cfb4552
Marcus Jakobsson <marcus.jakobsson@vertel.se>
2022-06-10 08:52:54 +0000

    sale_order_deadline_task: Added a diffrent logic for finding how many tasks will be created by a sale order.

--- sale_order_deadline_task/models/sale_order.py
+++ sale_order_deadline_task/models/sale_order.py
@@ -65,13 +65,99 @@ class SaleOrderLine(models.Model):
         res = super(SaleOrderLine, self)._timesheet_create_task_prepare_values(project)
         res.update({
             'date_deadline': self.date_deadline,
-        })
-        amount_of_tasks = self.env['project.task'].search_count([('date_deadline' ,'=', self.date_deadline)])
-        icp = self.env['ir.config_parameter'].sudo()
-        deadline_max_tasks = icp.get_param('sale_order_deadline_task.deadline_max_tasks', default=10)
-        if amount_of_tasks > int(deadline_max_tasks):
-            raise UserError(_("You have a lot of task set for this deadline"))
-                
+        })                
         return res
-        
+    
+    def _timesheet_service_generation(self):
+        """ For service lines, create the task or the project. If already exists, it simply links
+            the existing one to the line.
+            Note: If the SO was confirmed, cancelled, set to draft then confirmed, avoid creating a
+            new project/task. This explains the searches on 'sale_line_id' on project/task. This also
+            implied if so line of generated task has been modified, we may regenerate it.
+        """
+        before_create_amount_of_tasks = self.env['project.task'].search_count([('date_deadline' ,'=', self.order_id.date_deadline)])
 
+        so_line_task_global_project = self.filtered(lambda sol: sol.is_service and sol.product_id.service_tracking == 'task_global_project')
+        so_line_new_project = self.filtered(lambda sol: sol.is_service and sol.product_id.service_tracking in ['project_only', 'task_in_project'])
+
+        # search so lines from SO of current so lines having their project generated, in order to check if the current one can
+        # create its own project, or reuse the one of its order.
+        map_so_project = {}
+        if so_line_new_project:
+            order_ids = self.mapped('order_id').ids
+            so_lines_with_project = self.search([('order_id', 'in', order_ids), ('project_id', '!=', False), ('product_id.service_tracking', 'in', ['project_only', 'task_in_project']), ('product_id.project_template_id', '=', False)])
+            map_so_project = {sol.order_id.id: sol.project_id for sol in so_lines_with_project}
+            so_lines_with_project_templates = self.search([('order_id', 'in', order_ids), ('project_id', '!=', False), ('product_id.service_tracking', 'in', ['project_only', 'task_in_project']), ('product_id.project_template_id', '!=', False)])
+            map_so_project_templates = {(sol.order_id.id, sol.product_id.project_template_id.id): sol.project_id for sol in so_lines_with_project_templates}
+
+        # search the global project of current SO lines, in which create their task
+        map_sol_project = {}
+        if so_line_task_global_project:
+            map_sol_project = {sol.id: sol.product_id.with_company(sol.company_id).project_id for sol in so_line_task_global_project}
+
+        def _can_create_project(sol):
+            if not sol.project_id:
+                if sol.product_id.project_template_id:
+                    return (sol.order_id.id, sol.product_id.project_template_id.id) not in map_so_project_templates
+                elif sol.order_id.id not in map_so_project:
+                    return True
+            return False
+
+        def _determine_project(so_line):
+            """Determine the project for this sale order line.
+            Rules are different based on the service_tracking:
+
+            - 'project_only': the project_id can only come from the sale order line itself
+            - 'task_in_project': the project_id comes from the sale order line only if no project_id was configured
+              on the parent sale order"""
+
+            if so_line.product_id.service_tracking == 'project_only':
+                return so_line.project_id
+            elif so_line.product_id.service_tracking == 'task_in_project':
+                return so_line.order_id.project_id or so_line.project_id
+
+            return False
+
+        # task_global_project: create task in global project
+        for so_line in so_line_task_global_project:
+            if not so_line.task_id:
+                if map_sol_project.get(so_line.id):
+                    so_line._timesheet_create_task(project=map_sol_project[so_line.id])
+
+        # project_only, task_in_project: create a new project, based or not on a template (1 per SO). May be create a task too.
+        # if 'task_in_project' and project_id configured on SO, use that one instead
+        for so_line in so_line_new_project:
+            project = _determine_project(so_line)
+            if not project and _can_create_project(so_line):
+                project = so_line._timesheet_create_project()
+                if so_line.product_id.project_template_id:
+                    map_so_project_templates[(so_line.order_id.id, so_line.product_id.project_template_id.id)] = project
+                else:
+                    map_so_project[so_line.order_id.id] = project
+            elif not project:
+                # Attach subsequent SO lines to the created project
+                so_line.project_id = (
+                    map_so_project_templates.get((so_line.order_id.id, so_line.product_id.project_template_id.id))
+                    or map_so_project.get(so_line.order_id.id)
+                )
+            if so_line.product_id.service_tracking == 'task_in_project':
+                if not project:
+                    if so_line.product_id.project_template_id:
+                        project = map_so_project_templates[(so_line.order_id.id, so_line.product_id.project_template_id.id)]
+                    else:
+                        project = map_so_project[so_line.order_id.id]
+                if not so_line.task_id:
+                    so_line._timesheet_create_task(project=project)
+        # ~ after_create_amount_of_tasks = self.env['project.task'].search_count([('date_deadline' ,'=', self.order_id.date_deadline)])
+        # ~ _logger.warning(f"{after_create_amount_of_tasks=}")
+        # ~ _logger.warning(f"{self.task_id}")
+        _logger.warning(f"{len(self.task_id)}")
+        icp = self.env['ir.config_parameter'].sudo()
+        created_tasks_amount = len(self.task_id)
+        total_tasks = created_tasks_amount + before_create_amount_of_tasks
+        deadline_max_tasks = icp.get_param('sale_order_deadline_task.deadline_max_tasks', default=10)
+        if total_tasks > int(deadline_max_tasks):
+            # ~ raise UserError(_(f"When confirming this sale order you have tried to create {created_tasks_amount} tasks.\nThere were already {before_create_amount_of_tasks} with the deadline {self.order_id.date_deadline}.\nThis will result in {total_tasks} tasks with the same deadline, which is more than the max allowed of {deadline_max_tasks}.\nKindly change deadline before confirming."))
+            raise UserError(_('When confirming this sale order you have tried to create %s tasks.\nThere were already %s  with the deadline %s .\nThis will result in %s  tasks with the same deadline, which is more than the max allowed of %s.\nKindly change deadline before confirming.'% (str(created_tasks_amount), str(before_create_amount_of_tasks), str(self.order_id.date_deadline),str(total_tasks), str(deadline_max_tasks))))
+            
+        # ~ _logger.warn(_("Couldn't match line (id %s) against existing transfer item!\nlines:%s\ntransfer items:%s") % (line['id'], lines, wizard.item_ids.read()))
