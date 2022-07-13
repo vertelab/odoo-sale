404f21e3550caf7da91c15810e43623aed2166d3
Miracle <miracleaayodele@gmail.com>
2022-07-13 09:59:57 +0000

    filter on pivot

--- sale_project_task_description/models/project.py
+++ sale_project_task_description/models/project.py
@@ -6,6 +6,6 @@ class ProjectTask(models.Model):
 
     object_description = fields.Char(string="Objektbeskrivning")
 
-    product_id = fields.Many2one(related='sale_line_id.product_id')
+    product_id = fields.Many2one(related='sale_line_id.product_id', store=True)
     
-    categ_id = fields.Many2one(related='product_id.categ_id')
+    categ_id = fields.Many2one(related='product_id.categ_id', store=True)
