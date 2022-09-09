b997b7a83b06543fa8b98949f7f1120c1734acdc
iammiracle <miracleaayodele@gmail.com>
2022-09-09 16:51:53 +0200

    sale order task estimate for product variants

--- sale_order_task_estimate_time/models/product.py
+++ sale_order_task_estimate_time/models/product.py
@@ -1,9 +1,34 @@
 from odoo import fields, models, api, _
 
 import logging
+
 _logger = logging.getLogger(__name__)
+
 
 class Product(models.Model):
     _inherit = 'product.template'
 
+    estimated_time = fields.Float(string='Estimated Time', compute='_compute_estimated_time',
+                                  inverse='_set_estimated_time')
+
+    @api.depends_context('company')
+    @api.depends('product_variant_ids', 'product_variant_ids.estimated_time')
+    def _compute_estimated_time(self):
+        unique_variants = self.filtered(lambda product_template: len(product_template.product_variant_ids) == 1)
+        for template in unique_variants:
+            template.estimated_time = template.product_variant_ids.estimated_time
+        for template in (self - unique_variants):
+            template.estimated_time = 0.0
+
+    def _set_estimated_time(self):
+        for template in self:
+            if len(template.product_variant_ids) == 1:
+                template.product_variant_ids.estimated_time = template.estimated_time
+
+
+class ProductProduct(models.Model):
+    _inherit = 'product.product'
+
     estimated_time = fields.Float(string='Estimated Time')
+
+
--- sale_order_task_estimate_time/models/sale_order.py
+++ sale_order_task_estimate_time/models/sale_order.py
@@ -1,8 +1,8 @@
 from odoo import fields, models, api, _
 
 import logging
-_logger = logging.getLogger(__name__)
 
+_logger = logging.getLogger(__name__)
 
 
 class SaleOrderLine(models.Model):
@@ -10,16 +10,15 @@ class SaleOrderLine(models.Model):
 
     estimated_time = fields.Float(string="Estimated Time", copy=False)
 
-
     def _convert_qty_company_hours(self, dest_company):
         company_time_uom_id = dest_company.project_time_mode_id
         if self.product_uom.id != company_time_uom_id.id and self.product_uom.category_id.id == company_time_uom_id.category_id.id:
-            planned_hours = self.product_uom._compute_quantity(self.product_uom_qty, company_time_uom_id) * self.estimated_time
+            planned_hours = self.product_uom._compute_quantity(self.product_uom_qty,
+                                                               company_time_uom_id) * self.estimated_time
         else:
             planned_hours = self.product_uom_qty * self.estimated_time
         return planned_hours
 
-    
     @api.onchange('product_id')
     def product_estimated_time(self):
         if self.product_id:
