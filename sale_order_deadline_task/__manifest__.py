11e9e27a0f32028a869b246b098b5796da8e2e47
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2022-10-19 14:58:25 +0200

    Update __manifest__.py

--- sale_order_deadline_task/__manifest__.py
+++ sale_order_deadline_task/__manifest__.py
@@ -1,8 +1,9 @@
+
 # -*- coding: utf-8 -*-
 ##############################################################################
 #
-#    Odoo, Open Source Enterprise Management Solution, third party addon
-#    Copyright (C) 2020- Vertel AB (<http://vertel.se>).
+#    Odoo SA, Open Source Management Solution, third party addon
+#    Copyright (C) 2022- Vertel AB (<https://vertel.se>).
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
@@ -15,16 +16,27 @@
 #    GNU Affero General Public License for more details.
 #
 #    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#    along with this program. If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
+
 {
-    'name': 'Task Sale Order DeadLine',
+    'name': 'Sale: Order Deadline Task',
     'version': '14.0.1.0',
+    # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
+    'summary': '',
+    'category': 'Sales',
+    'description': """
+    
+    """,
+    #'sequence': '1',
+    'author': 'Vertel AB',
+    'website': 'https://vertel.se/apps/odoo-sale/sale_order_deadline_task',
+    'images': ['static/description/banner.png'], # 560x280 px.
     'license': 'AGPL-3',
-    'author': ' Vertel AB',
-    'website': 'https://vertel.se',
-    'category': 'sale',
+    'contributor': '',
+    'maintainer': 'Vertel AB',
+    'repository': 'https://github.com/vertelab/odoo-sale',
     'depends': ['sale_management', 'sale_project', 'project'],
     'data': [
         'security/ir.model.access.csv',
