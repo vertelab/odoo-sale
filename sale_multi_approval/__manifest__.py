17425d30b60d2dc76682b3dcb4bb5a5a7568d871
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2022-10-19 14:19:34 +0200

    Update __manifest__.py

--- sale_multi_approval/__manifest__.py
+++ sale_multi_approval/__manifest__.py
@@ -1,21 +1,41 @@
 # -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Odoo SA, Open Source Management Solution, third party addon
+#    Copyright (C) 2022- Vertel AB (<https://vertel.se>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
 {
-    'name': "sale_multi_approval",
-
-    'summary': """
-        Adds multi aproval functionality using bankid for sale orders""",
-
+    'name': 'Sale: Multi Approval',
+    'version': '14.0.0.0.0',
+    # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
+    'summary': 'Adds multi aproval functionality using bankid for sale orders.',
+    'category': 'Sales',
     'description': """
-        Adds multi aproval functionality using bankid for sale orders
+    Adds multi aproval functionality using bankid for sale orders.
     """,
-
-    'author': "Vertel AB",
-    'website': "www.vertel.se",
-
-    # Categories can be used to filter modules in modules listing
-    'category': 'sale',
-    'version': '14.0.0.1',
-
+    #'sequence': '1',
+    'author': 'Vertel AB',
+    'website': 'https://vertel.se/apps/odoo-sale/sale_multi_approval',
+    'images': ['static/description/banner.png'], # 560x280 px.
+    'license': 'AGPL-3',
+    'contributor': '',
+    'maintainer': 'Vertel AB',
+    'repository': 'https://github.com/vertelab/odoo-sale',
     # any module necessary for this one to work correctly
     'depends': ['base', 'sale', 'rest_base', 'rest_signport', 'res_user_groups_skogsstyrelsen', "partner_ssn"],
 
@@ -32,3 +52,4 @@
         'views/sale_inherited.xml',
     ],
 }
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
