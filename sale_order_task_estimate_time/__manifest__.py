71415e5e06a6935de53171b3def38ed0ca581f9c
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2022-10-19 16:45:51 +0200

    Update __manifest__.py

--- sale_order_task_estimate_time/__manifest__.py
+++ sale_order_task_estimate_time/__manifest__.py
@@ -2,7 +2,7 @@
 ##############################################################################
 #
 #    Odoo SA, Open Source Management Solution, third party addon
-#    Copyright (C) 2021- Vertel AB (<https://vertel.se>).
+#    Copyright (C) 2022- Vertel AB (<https://vertel.se>).
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
@@ -20,16 +20,18 @@
 ##############################################################################
 
 {
-    'name': 'Sale Order Task Time Estimate',
+    'name': 'Sale: Order Task Time Estimate',
     'version': '14.0.0.1.0',
     # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
-    'summary': 'Sale Order Task Time Estimate',
-    'category': 'Marketing',
+    'summary': 'This module adds time estimate to product and sale order line then to task.',
+    'category': 'Sales',
     'description': """
-        This module adds time estimate to product and sale order line then to task.
+    This module adds time estimate to product and sale order line then to task.
     """,
+    #'sequence': '1',
     'author': 'Vertel AB',
-    'website': 'https://vertel.se/apps/',
+    'website': 'https://vertel.se/apps/odoo-sale/sale_order_task_estimate_time',
+    'images': ['static/description/banner.png'], # 560x280 px.
     'license': 'AGPL-3',
     'contributor': '',
     'maintainer': 'Vertel AB',
@@ -42,3 +44,4 @@
     'installable': True,
     'auto_install': False,
 }
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
