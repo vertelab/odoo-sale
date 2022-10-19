8f203bfd3f98849a492b2cbc8f4a18f92069384c
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2022-10-19 15:38:05 +0200

    Update __manifest__.py

--- sale_order_fix_create_invoice_wizard/__manifest__.py
+++ sale_order_fix_create_invoice_wizard/__manifest__.py
@@ -1,21 +1,41 @@
 # -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Odoo SA, Open Source Management Solution, third party addon
+#    Copyright (C) 2022- Vertel AB (<https://vertel.se>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
 {
-    'name': "Fix Sale Order Wizard",
-
-    'summary': """
-        Fix Sale Order Wizard""",
-
-    'description': """
-       Fix Sale Order Wizard
-    """,
-
-    'author': "Vertel AB",
-    'website': "www.vertel.se",
-
-    # Categories can be used to filter modules in modules listing
-    'category': 'sale',
+    'name': 'Sale: Order Fix Create Invoice Wizard',
     'version': '14.0.0.1',
-
+    # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
+    'summary': 'Fix Sale Order Wizard',
+    'category': 'Sales',
+    'description': """
+    Fix Sale Order Wizard
+    """,
+    #'sequence': '1',
+    'author': 'Vertel AB',
+    'website': 'https://vertel.se/apps/odoo-sale/sale_order_fix_create_invoice_wizard',
+    'images': ['static/description/banner.png'], # 560x280 px.
+    'license': 'AGPL-3',
+    'contributor': '',
+    'maintainer': 'Vertel AB',
+    'repository': 'https://github.com/vertelab/odoo-sale',
     # any module necessary for this one to work correctly
     'depends': ['base', 'sale'],
 
@@ -24,3 +44,4 @@
         #'views/sale_inherited.xml',
     ],
 }
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
