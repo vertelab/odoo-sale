83fbd62aaaea04755c7358309eedc6471f9cf575
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2022-10-19 16:05:27 +0200

    Update __manifest__.py

--- sale_order_print_template/__manifest__.py
+++ sale_order_print_template/__manifest__.py
@@ -2,7 +2,7 @@
 ##############################################################################
 #
 #    Odoo SA, Open Source Management Solution, third party addon
-#    Copyright (C) 2021- Vertel AB (<https://vertel.se>).
+#    Copyright (C) 2022- Vertel AB (<https://vertel.se>).
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
@@ -20,15 +20,18 @@
 ##############################################################################
 
 {
-    'name': 'Sale Order Print Template',
+    'name': 'Sale: Order Print Template',
     'version': '14.0.0.1.0',
-    'summary': 'Hide Quotation Price Details',
+    # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
+    'summary': ' This module hide price details for quotations.',
     'category': 'Sales',
     'description': """
-        This module hide price details for quotations.
+     This module hide price details for quotations.
     """,
+    #'sequence': '1',
     'author': 'Vertel AB',
-    'website': 'https://vertel.se/apps/',
+    'website': 'https://vertel.se/apps/odoo-sale/sale_order_print_template',
+    'images': ['static/description/banner.png'], # 560x280 px.
     'license': 'AGPL-3',
     'contributor': '',
     'maintainer': 'Vertel AB',
@@ -40,3 +43,4 @@
     'installable': True,
     'auto_install': False,
 }
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
