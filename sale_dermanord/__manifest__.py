9d2524dc2d400ae7d28440513cc1fb3a70eb86f6
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2022-10-19 14:03:49 +0200

    Update __manifest__.py

--- sale_dermanord/__manifest__.py
+++ sale_dermanord/__manifest__.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
 #
-#    OpenERP, Open Source Management Solution, third party addon
-#    Copyright (C) 2017- Vertel AB (<http://vertel.se>).
+#    Odoo SA, Open Source Management Solution, third party addon
+#    Copyright (C) 2022- Vertel AB (<https://vertel.se>).
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
@@ -15,14 +15,16 @@
 #    GNU Affero General Public License for more details.
 #
 #    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#    along with this program. If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
 
 {
-    'name': 'Sale Dermanord',
-    'version': '1.0',
-    'category': '',
+    'name': 'Sale: Dermanord',
+    'version': '14.0.0.0.0',
+    # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
+    'summary': 'Special order imports for Dermanord AB.',
+    'category': 'Sales',
     'description': """
 Special order imports for Dermanord AB
 ======================================
@@ -35,9 +37,10 @@ Special order imports for Dermanord AB
 * Tailwide AB (Narutligt snygg) (Url)
 
 """,
+    #'sequence': '1',
     'author': 'Vertel AB',
-    'license': 'AGPL-3',
-    'website': 'http://www.vertel.se',
+    'website': 'https://vertel.se/apps/odoo-sale/sale_dermanord',
+
     'depends': ['sale', 'sale_management'],
     'external_dependencies': {
         'python': ['xlrd', 'pdfminer', 'unicodecsv'],
@@ -49,4 +52,4 @@ Special order imports for Dermanord AB
     'application': False,
     'installable': True,
 }
-# vim:expandtab:smartindent:tabstop=4s:softtabstop=4:shiftwidth=4:
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
