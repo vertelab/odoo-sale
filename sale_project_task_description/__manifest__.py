05aff1e70834f9c52af857cbd7b491cea29ec419
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2022-10-20 11:05:50 +0200

    Update __manifest__.py

--- sale_project_task_description/__manifest__.py
+++ sale_project_task_description/__manifest__.py
@@ -2,7 +2,7 @@
 ##############################################################################
 #
 #    Odoo SA, Open Source Management Solution, third party addon
-#    Copyright (C) 2021- Vertel AB (<https://vertel.se>).
+#    Copyright (C) 2022- Vertel AB (<https://vertel.se>).
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
@@ -20,16 +20,22 @@
 ##############################################################################
 
 {
-    'name': 'Sale Project Task Description',
-    'version': '14.0.0.0.0',
-    'summary': 'Sale Project Task Description',
-    'category': 'Marketing',
+    'name': 'Sale: Project Task Description',
+    'version': '14.0.0.1.0',
+    # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
+    'summary': 'Adds description to sale project task.',
+    'category': 'Sales',
     'description': """
-        Adds description to sale project task
+    This module adds description to sale project task.
     """,
+    #'sequence': '1',
     'author': 'Vertel AB',
-    'website': 'https://vertel.se',
+    'website': 'https://vertel.se/apps/odoo-sale/sale_project_task_description',
+    'images': ['static/description/banner.png'], # 560x280 px.
     'license': 'AGPL-3',
+    'contributor': '',
+    'maintainer': 'Vertel AB',
+    'repository': 'https://github.com/vertelab/odoo-sale',
     'depends': ['sale_management', 'project', 'sale_project'],
     'data': [
         'views/project_view.xml',
@@ -39,3 +45,4 @@
     'installable': True,
     'auto_install': False,
 }
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
