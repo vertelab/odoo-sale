6098b3d5c56b6d605ac03a1f114e71df2703c7cc
iammiracle <miracleaayodele@gmail.com>
2023-01-12 14:52:45 +0100

    sale order pivot report

---
+++ sale_order_pivot_report/__manifest__.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Odoo SA, Open Source Management Solution, third party addon
+#    Copyright (C) 2022- Vertel AB (<https://vertel.se>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    'name': 'Sale: Sale Order Pivot Report',
+    'version': '14.0.0.1.0',
+    'summary': ' This module add more sale order fields to sales order pivot view.',
+    'category': 'Sales',
+    'description': """
+        This module add more sale order fields to sales order pivot view
+    """,
+    'author': 'Vertel AB',
+    'website': 'https://vertel.se/apps/odoo-sale/sale_order_print_template',
+    'images': ['static/description/banner.png'],
+    'license': 'AGPL-3',
+    'contributor': '',
+    'maintainer': 'Vertel AB',
+    'repository': 'https://github.com/vertelab/odoo-sale',
+    'depends': ['sale', 'sale_stock'],
+    'data': [
+        'views/sale_report_view.xml'
+    ],
+    'installable': True,
+    'auto_install': False,
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
---
+++ sale_order_pivot_report/models/sale_report.py
@@ -0,0 +1,82 @@
+from odoo import tools
+from odoo import api, fields, models
+
+
+class SaleReport(models.Model):
+    _inherit = "sale.report"
+
+    effective_date = fields.Date('Effective Date', readonly=True)
+
+    def _select_sale(self, fields=None):
+        if not fields:
+            fields = {}
+        select_ = """
+            coalesce(min(l.id), -s.id) as id,
+            l.product_id as product_id,
+            t.uom_id as product_uom,
+            CASE WHEN l.product_id IS NOT NULL THEN sum(l.product_uom_qty / u.factor * u2.factor) ELSE 0 END as product_uom_qty,
+            CASE WHEN l.product_id IS NOT NULL THEN sum(l.qty_delivered / u.factor * u2.factor) ELSE 0 END as qty_delivered,
+            CASE WHEN l.product_id IS NOT NULL THEN sum(l.qty_invoiced / u.factor * u2.factor) ELSE 0 END as qty_invoiced,
+            CASE WHEN l.product_id IS NOT NULL THEN sum(l.qty_to_invoice / u.factor * u2.factor) ELSE 0 END as qty_to_invoice,
+            CASE WHEN l.product_id IS NOT NULL THEN sum(l.price_total / CASE COALESCE(s.currency_rate, 0) WHEN 0 THEN 1.0 ELSE s.currency_rate END) ELSE 0 END as price_total,
+            CASE WHEN l.product_id IS NOT NULL THEN sum(l.price_subtotal / CASE COALESCE(s.currency_rate, 0) WHEN 0 THEN 1.0 ELSE s.currency_rate END) ELSE 0 END as price_subtotal,
+            CASE WHEN l.product_id IS NOT NULL THEN sum(l.untaxed_amount_to_invoice / CASE COALESCE(s.currency_rate, 0) WHEN 0 THEN 1.0 ELSE s.currency_rate END) ELSE 0 END as untaxed_amount_to_invoice,
+            CASE WHEN l.product_id IS NOT NULL THEN sum(l.untaxed_amount_invoiced / CASE COALESCE(s.currency_rate, 0) WHEN 0 THEN 1.0 ELSE s.currency_rate END) ELSE 0 END as untaxed_amount_invoiced,
+            count(*) as nbr,
+            s.name as name,
+            s.date_order as date,
+            s.effective_date as effective_date,
+            s.state as state,
+            s.partner_id as partner_id,
+            s.user_id as user_id,
+            s.company_id as company_id,
+            s.campaign_id as campaign_id,
+            s.medium_id as medium_id,
+            s.source_id as source_id,
+            extract(epoch from avg(date_trunc('day',s.date_order)-date_trunc('day',s.create_date)))/(24*60*60)::decimal(16,2) as delay,
+            t.categ_id as categ_id,
+            s.pricelist_id as pricelist_id,
+            s.analytic_account_id as analytic_account_id,
+            s.team_id as team_id,
+            p.product_tmpl_id,
+            partner.country_id as country_id,
+            partner.industry_id as industry_id,
+            partner.commercial_partner_id as commercial_partner_id,
+            CASE WHEN l.product_id IS NOT NULL THEN sum(p.weight * l.product_uom_qty / u.factor * u2.factor) ELSE 0 END as weight,
+            CASE WHEN l.product_id IS NOT NULL THEN sum(p.volume * l.product_uom_qty / u.factor * u2.factor) ELSE 0 END as volume,
+            l.discount as discount,
+            CASE WHEN l.product_id IS NOT NULL THEN sum((l.price_unit * l.product_uom_qty * l.discount / 100.0 / CASE COALESCE(s.currency_rate, 0) WHEN 0 THEN 1.0 ELSE s.currency_rate END))ELSE 0 END as discount_amount,
+            s.id as order_id
+        """
+
+        for field in fields.values():
+            select_ += field
+        return select_
+
+    def _group_by_sale(self, groupby=''):
+        groupby_ = """
+            l.product_id,
+            l.order_id,
+            t.uom_id,
+            t.categ_id,
+            s.name,
+            s.date_order,
+            s.effective_date,
+            s.partner_id,
+            s.user_id,
+            s.state,
+            s.company_id,
+            s.campaign_id,
+            s.medium_id,
+            s.source_id,
+            s.pricelist_id,
+            s.analytic_account_id,
+            s.team_id,
+            p.product_tmpl_id,
+            partner.country_id,
+            partner.industry_id,
+            partner.commercial_partner_id,
+            l.discount,
+            s.id %s
+        """ % (groupby)
+        return groupby_
