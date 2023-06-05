7c8a6831f73cb03677b0ec1d785b38d5c2dbf16f
Kriss-Ellen Svensson <kriss-ellen.svensson@vertel.se>
2023-06-05 10:00:10 +0000

    Remowed a function and added a new that is listening to all the task that is conected to sale.order.line

--- sale_project_task_description/models/sale_order.py
+++ sale_project_task_description/models/sale_order.py
@@ -1,18 +1,21 @@
 from odoo import models, fields, _, api
-
-# ~ class ProductTemplate(models.Model):
-    # ~ _inherit = 'product.template'
-    # ~ work_description = fields.Char(string="Work Description")
-
-# ~ class ProductProduct(models.Model):
-    # ~ _inherit = 'product.product'
-    # ~ work_description = fields.Char(string="Work Description")
     
 
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
     work_description = fields.Char(string="Work Description")
+    
+    def _action_confirm(self):
+        res = super()._action_confirm()
+        for record in self:
+            for task in record.order_line.tasks_ids:
+                    if not task.description:
+                        task.description = task.sale_line_id.work_description
+                    if not task.object_description:
+                         task.object_description = task.sale_line_id.name
+                    
+        return res
 
 
 class SaleOrderLine(models.Model):
@@ -20,16 +23,26 @@ class SaleOrderLine(models.Model):
     
     work_description = fields.Char(string="Work Description")
     
+    tasks_ids = fields.One2many('project.task','sale_line_id')
+    
     # ~ @api.onchange('product_id')
     # ~ def product_id_change_set_work_description(self):
         # ~ for record in self: 
             # ~ if record.product_id.work_description:
                 # ~ record.work_description = record.product_id.work_description
     
-    def _timesheet_create_task_prepare_values(self, project):
-        res = super(SaleOrderLine, self)._timesheet_create_task_prepare_values(project)
-        res.update({
-            'description': self.work_description,
-            'object_description': self.name,
-        })
-        return res
+    # ~ def _timesheet_create_task_prepare_values(self, project):
+        # ~ for record in self:
+            # ~ res = super(SaleOrderLine, self)._timesheet_create_task_prepare_values(project)
+        
+            # ~ _logger.warning(f"{record.tasks_ids=}")
+            # ~ for task in record.tasks_ids:
+            # ~ _logger.error(f"{self.work_description}")
+                # ~ res.update({
+                    # ~ 'description': self.work_description,
+                    # ~ 'object_description': self.name,
+                # ~ })
+            # ~ _logger.error(f"{res=}")
+        # ~ return res
+        
+        
