0b171f0c700526d2ed8a7822d5dedc15a0d837b1
iammiracle <miracleaayodele@gmail.com>
2024-06-14 12:00:12 +0200

    bug fix

--- sale_multi_approval/models/sale_order.py
+++ sale_multi_approval/models/sale_order.py
@@ -207,6 +207,7 @@ class SaleOrder(models.Model):
                 current_user_line = rec.approval_ids.filtered(lambda line: line.approver_id.id == self.env.uid)[0]
                 if current_user_line.approval_status:
                     rec.is_approved = True
+                    return
             rec.is_approved = False
 
     @api.depends('approval_ids')
