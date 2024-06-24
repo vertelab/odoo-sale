9b280d248e880dd037a2cbc56482d5f4d3b91453
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2024-06-24 16:43:50 +0200

    Update __manifest__.py
    Upgrading 14 >> 16

--- sale_46elks_sms/__manifest__.py
+++ sale_46elks_sms/__manifest__.py
@@ -21,8 +21,8 @@
 
 {
     'name': 'Sale: 46elks SMS',
-    'version': '16.0.0.0.0',
-    # Version ledger: 16.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
+    'version': '14.0.0.0.0',
+    # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
     'summary': '',
     'category': 'Sales',
     'description': """
