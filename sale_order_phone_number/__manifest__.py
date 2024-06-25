056f8c23901f4b1a869d01c0d1fbc18bbe2e2933
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2024-06-25 08:48:51 +0200

    Update __manifest__.py
    updating to odoo 16

--- sale_order_phone_number/__manifest__.py
+++ sale_order_phone_number/__manifest__.py
@@ -21,8 +21,8 @@
 
 {
     'name': 'Sale: Order Phone Number',
-    'version': '14.0.0.1.0',
-    # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
+    'version': '16.0.0.1.0',
+    # Version ledger: 16.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
     'summary': 'This module adds phone number to sale order.',
     'category': 'Sales',
     'description': """
