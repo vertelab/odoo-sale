010246263ef89b74299330a73d588283c325a7c8
Jakob Krabbe <jakob@syscare.se>
2024-08-15 12:32:31 +0000

    odoo-sale: fine tuning __manifest__.py for apps.odoo.com ...

--- sale_46elks_sms/__manifest__.py
+++ sale_46elks_sms/__manifest__.py
@@ -21,8 +21,8 @@
 
 {
     'name': 'Sale: 46elks SMS',
-    'version': '16.0.0.0.0',
-    # Version ledger: 16.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
+    'version': '0.1',
+    # Version ledger: XX.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
     'summary': '',
     'category': 'Sales',
     'description': """
