fa5b6fd263e5f0d3350e6fc74a17f21d36eb2e19
iammiracle <miracleaayodele@gmail.com>
2025-05-28 16:01:01 +0200

    [WIP]implementation of sign with bank id via the sale order flow

---
+++ sale_sign_bankid/__manifest__.py
@@ -0,0 +1,22 @@
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Sign Sale Order with BankID",
+    "summary": """
+        Sign Sale Order with BankID
+    """,
+    "version": "18.0.1.0.0",
+    "license": "AGPL-3",
+    "website": "https://github.com/vertel/odoo-sale",
+    "depends": ['sign_bankid', 'sale'],
+    "data": [
+        'views/sale_order_view.xml',
+        'views/sale_portal_templates.xml',
+    ],
+    'assets': {
+        'web.assets_backend': [
+
+        ],
+    }
+
+}
---
+++ sale_sign_bankid/models/sale_order.py
@@ -0,0 +1,27 @@
+import json
+import base64
+import httpx
+import requests
+from datetime import datetime
+from odoo import models, fields, api, _
+
+
+
+class SaleOrder(models.Model):
+    _name = "sale.order"
+    _inherit = ["res.bankid", "sale.order"]
+
+    def action_confirm(self):
+        return {
+            'type': 'ir.actions.client',
+            'tag': 'bankid_sign_modal',
+            'target': 'new',
+            'context': {
+                'default_record_id': self.id,
+                'default_record_model': self._name,
+            }
+        }
+        # return super().action_confirm()
+
+
+
