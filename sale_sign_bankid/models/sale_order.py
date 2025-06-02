d736818df2c873064089acc7eaa23a34932ae8da
iammiracle <miracleaayodele@gmail.com>
2025-06-02 13:39:30 +0200

    override sale order confirm action - action_confirm

--- sale_sign_bankid/models/sale_order.py
+++ sale_sign_bankid/models/sale_order.py
@@ -12,16 +12,19 @@ class SaleOrder(models.Model):
     _inherit = ["res.bankid", "sale.order"]
 
     def action_confirm(self):
-        return {
-            'type': 'ir.actions.client',
-            'tag': 'bankid_sign_modal',
-            'target': 'new',
-            'context': {
-                'default_record_id': self.id,
-                'default_record_model': self._name,
+        if not self._has_user_signed_rec():
+            return {
+                'type': 'ir.actions.client',
+                'tag': 'bankid_sign_modal',
+                'target': 'new',
+                'context': {
+                    'default_record_id': self.id,
+                    'default_record_model': self._name,
+                    'callback_method': 'action_confirm',
+                }
             }
-        }
-        # return super().action_confirm()
+        else:
+            return super().action_confirm()
 
 
 
